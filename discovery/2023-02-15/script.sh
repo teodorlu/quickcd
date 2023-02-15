f7b779313d96a655a9a1b634cabc9bad1d2993ab
Teodor Heggelund <git@teod.eu>
2023-02-15 14:00:46 +0100

    example: eval bb stdout from bash

--- discovery/2023-02-15/script.sh
+++ discovery/2023-02-15/script.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 eval_lines() {
-    # run a subcommand, eval each line in the current process
-    # take each line from this function's stdin
     while read -r line; do
         eval "$line"
     done
 }
 
-eval_lines <<EOF
-echo a line
-echo another
+bb_script=$(cat <<EOF
+(doseq [n (range 5)]
+  (println "echo" n))
 EOF
+)
+
+bb -e "$bb_script" | eval_lines
