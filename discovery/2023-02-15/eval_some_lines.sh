62144e02a9c93fd3c9c2ecfeab144a2fe5ae6591
Teodor Heggelund <git@teod.eu>
2023-02-15 14:44:24 +0100

    Example with shelleval

--- discovery/2023-02-15/eval_some_lines.sh
+++ discovery/2023-02-15/eval_some_lines.sh
@@ -17,12 +17,25 @@ EOF
 
 eval_some_lines() {
   while read -r line; do
-    eval "$line"
+    if echo "$line" | grep '^!SHELLEVAL'; then
+      echo IF
+      cmd=$(echo "$line" | sed 's/!SHELLEVAL//g')
+      eval "$cmd"
+    else
+      echo ELSE
+      echo "$line"
+    fi
   done
 }
 
 demo_2() {
-  echo 2
+  eval_some_lines <<EOF
+normal passthrough
+!SHELLEVAL echo "this is fine"
+!SHELLEVAL cd ..
+!SHELLEVAL pwd
+another passthrough
+EOF
 }
 
 demo_2
